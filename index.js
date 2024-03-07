eec7ab9a242e4017144fe61ec93dcd816063e1bb
itsmefaishal <faishal.rahman.79@gmail.com>
2024-03-07 18:14:03 +0530

    added dotenv file

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config();
 const userRoutes = require("./routes/users.routes");
+const userName = process.env.APP_USER_NAME;
+const password = process.env.APP_PASSWORD;
+
 app.use(express.json());
+app.use("/api/user", userRoutes);
 
 app.get("/", function (req, res) {
   res.send("Hello World from crud by faishal");
 });
 
-app.use("/api/user", userRoutes);
 
 mongoose
   .connect(
-    "mongodb+srv://faishalrahman79:h1WGILJdjK7lFEn4@cluster0.qkqnz69.mongodb.net/CRUD-APP-API?retryWrites=true&w=majority&appName=Cluster0"
+    `mongodb+srv://${userName}:${password}@cluster0.qkqnz69.mongodb.net/CRUD-APP-API?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     console.log("connected to mongodb");
